6c12ca46658a2c00440a1566492a6d325ca9194c
Mimoza Mickoska <63194172+Mimziphery@users.noreply.github.com>
2022-08-11 15:10:43 +0200

    project started

---
+++ PsychoSphere/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PsychoSphere
+{
+    public partial class mainMenu : Form
+    {
+        public mainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Form2 Form = new Form2();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
